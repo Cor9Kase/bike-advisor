25c61c84013ef1c8f818352c18a6a5ae6808942d
Cor9Kase <cornelius@skardigital.no>
2025-05-21 18:53:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', async () => {
 
     // --- Konstanter ---
-    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzCqV1dM4bBy_zpipqaDM3TEqrMNZ-RkpP2QvY0YMOUhvTIOrFZWFZGI-QnhMuNxiXA/exec'; // ERSTATT DENNE
+    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzaG6_GQeWYBKJ4etlTH1UWavFSPcU95aUXTcL9hEXYksWah7FLNpLYOemloMLZ4xMt/exec'; // ERSTATT DENNE
 
     // --- State Variabler ---
     let BikeCatalog = { evoOriginal: [] };
