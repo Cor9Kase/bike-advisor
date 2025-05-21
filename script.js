665ff2397938d6024b8f796aea9e7c50398bf4fb
Cor9Kase <cornelius@skardigital.no>
2025-05-21 17:51:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     // --- Konstanter ---
     // VIKTIG: Erstatt med din faktiske, publiserte Google Apps Script Web App URL
-    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzqqCUFgiCK3AxzRcs7EdCfHvsYYFF00WVoVx2BE4rAp6K4TW4Qp-NXK3PZsB0lH5i8/exec';
+    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyiGvf96nZhQJUREGtdNjKUDaiW2lj5jJxGTNVSrPZPziIcISRt02PcWsCYy9uTA5YS/exec';
 
     // --- State Variabler ---
     let BikeCatalog = {
